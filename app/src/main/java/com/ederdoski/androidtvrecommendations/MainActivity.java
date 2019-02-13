0313b69763d048b4d6b6b24c80a158eea9278048
Edermar Dominguez <ederdoski19@gmail.com>
2019-02-13 11:46:50 -0300

    Update MainActivity.java

--- app/src/main/java/com/ederdoski/androidtvrecommendations/MainActivity.java
+++ app/src/main/java/com/ederdoski/androidtvrecommendations/MainActivity.java
@@ -16,18 +16,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        String[] aExtra = {"extra_1", "extra_2", "extra_3"};
         ArrayList<LauncherRecommended> aVideo = new ArrayList<>();
 
         aVideo.add(new LauncherRecommended("1", "Titulo de Prueba  1", "Descripcion Prueba 1 ", "http://lorempixel.com/350/210/"));
         aVideo.add(new LauncherRecommended("2", "Titulo de Prueba  2", "Descripcion Prueba 2 ", "http://lorempixel.com/350/220/"));
         aVideo.add(new LauncherRecommended("3", "Titulo de Prueba  3", "Descripcion Prueba 3 ", "http://lorempixel.com/350/230/"));
 
-
         Intent recommendationIntent = new Intent(this, UpdateRecommendationsService.class);
-        recommendationIntent.putExtra("cardIcon", com.ederdoski.launcherRecommendations.R.mipmap.ic_launcher);
-        recommendationIntent.putExtra("cardWidth", getResources().getDimensionPixelSize(com.ederdoski.launcherRecommendations.R.dimen.card_width));
-        recommendationIntent.putExtra("cardHeight", getResources().getDimensionPixelSize(com.ederdoski.launcherRecommendations.R.dimen.card_height));
         recommendationIntent.putParcelableArrayListExtra("LauncherRecommended", aVideo);
+        recommendationIntent.putExtra("cardIcon", R.mipmap.ic_launcher);
+        recommendationIntent.putExtra("cardWidth", getResources().getDimensionPixelSize(R.dimen.card_width));
+        recommendationIntent.putExtra("cardHeight", getResources().getDimensionPixelSize(R.dimen.card_height));
+        recommendationIntent.putExtra("extra", "string_extra");
+        recommendationIntent.putExtra("array-extra", aExtra);
+        recommendationIntent.putExtra("toClass", MainActivity.class);
         startService(recommendationIntent);
+
     }
+
+    private String getIntent(Activity act) {
+        if(act.getIntent() != null) {
+            return act.getIntent().getStringExtra("extra");
+        }else{
+            return "null";
+        }
+    }
+
+    private String[] getIntentArray(Activity act) {
+        if(act.getIntent() != null){
+            return act.getIntent().getStringArrayExtra("array-extra");
+        }else{
+            return new String[0];
+        }
+    }
+
 }
